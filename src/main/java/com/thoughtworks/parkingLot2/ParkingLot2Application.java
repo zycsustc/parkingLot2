1c9e3a6b5d1f9c65ae85d8d0ab90eb4b006bf9ca
yichi <yichi.zhang@thoughtworks.com>
2020-08-30 00:49:05 +0800

    [yichi]-init project

---
+++ src/main/java/com/thoughtworks/parkingLot2/ParkingLot2Application.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.parkingLot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLot2Application.class, args);
+	}
+
+}
